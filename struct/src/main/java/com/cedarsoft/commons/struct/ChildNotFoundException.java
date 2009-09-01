b280d649e3a822600e03e164fa311ac89f38c601
Johannes Schneider <js@cedarsoft.com>
2009-09-01 14:13:11 +0200

    Struct: Child not found exception based on NotFoundException

--- struct/src/main/java/com/cedarsoft/commons/struct/ChildNotFoundException.java
+++ struct/src/main/java/com/cedarsoft/commons/struct/ChildNotFoundException.java
@@ -1,5 +1,6 @@
 package com.cedarsoft.commons.struct;
 
+import com.cedarsoft.NotFoundException;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -10,7 +11,7 @@ import org.jetbrains.annotations.NotNull;
  * @author <a href="http://johannes-schneider.info">Johannes Schneider</a> -
  *         <a href="http://www.xore.de">Xore Systems</a>
  */
-public class ChildNotFoundException extends RuntimeException {
+public class ChildNotFoundException extends NotFoundException {
   private final Path path;
 
   public ChildNotFoundException( @NotNull Path path ) {
