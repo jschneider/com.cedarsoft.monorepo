dcd824acd6786f3161b331be4acc29f3769ba63d
Johannes Schneider <js@cedarsoft.com>
2009-10-19 22:04:31 +0200

    Serialzing strategy useable as own serializer

--- serialization/src/main/java/com/cedarsoft/serialization/AbstractSerializingStrategy.java
+++ serialization/src/main/java/com/cedarsoft/serialization/AbstractSerializingStrategy.java
@@ -1,12 +1,16 @@
 package com.cedarsoft.serialization;
 
+import com.cedarsoft.lookup.Lookup;
+import org.jdom.Element;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 
 /**
  * @param <T> the type
  */
-public abstract class AbstractSerializingStrategy<T> implements SerializingStrategy<T> {
+public abstract class AbstractSerializingStrategy<T> extends AbstractSerializer<T> implements SerializingStrategy<T> {
   @NotNull
   @NonNls
   private final String id;
@@ -15,6 +19,7 @@ public abstract class AbstractSerializingStrategy<T> implements SerializingStrat
   private final Class<? extends T> supportedType;
 
   protected AbstractSerializingStrategy( @NotNull String id, @NotNull Class<? extends T> supportedType ) {
+    super( id );
     this.id = id;
     this.supportedType = supportedType;
   }
@@ -26,5 +31,18 @@ public abstract class AbstractSerializingStrategy<T> implements SerializingStrat
 
   public boolean supports( @NotNull Object object ) {
     return supportedType.isAssignableFrom( object.getClass() );
+  }
+
+  @NotNull
+  @Override
+  public Element serialize( @NotNull Element element, @NotNull T object, @NotNull Lookup context ) throws IOException {
+    serialize( element, object );
+    return element;
+  }
+
+  @NotNull
+  @Override
+  public T deserialize( @NotNull Element element, @NotNull Lookup context ) throws IOException {
+    return deserialize( element );
   }
 }
