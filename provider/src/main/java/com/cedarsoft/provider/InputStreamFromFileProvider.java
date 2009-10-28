e70b341f94e6160c6cd46e4b6dc94b2047d2b9a0
Johannes Schneider <js@cedarsoft.com>
2009-10-28 21:26:51 +0100

    Provider for input stream form file added

---
+++ provider/src/main/java/com/cedarsoft/provider/InputStreamFromFileProvider.java
@@ -0,0 +1,31 @@
+package com.cedarsoft.provider;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ *
+ */
+public class InputStreamFromFileProvider implements Provider<InputStream, IOException> {
+  @NotNull
+  private final File file;
+
+  public InputStreamFromFileProvider( @NotNull File file ) {
+    this.file = file;
+  }
+
+  @NotNull
+  public InputStream provide() throws IOException {
+    return new BufferedInputStream( new FileInputStream( file ) );
+  }
+
+  @NotNull
+  public String getDescription() {
+    return file.getAbsolutePath();
+  }
+}
