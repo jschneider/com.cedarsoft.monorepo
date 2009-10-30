4dd5b5cb1cb873ec22f06de8821f94e6d739d48d
Johannes Schneider <js@cedarsoft.com>
2009-10-30 10:26:24 +0100

    AssertUtils: Xml methods added

--- test-utils/src/main/java/com/cedarsoft/AssertUtils.java
+++ test-utils/src/main/java/com/cedarsoft/AssertUtils.java
@@ -1,9 +1,21 @@
 package com.cedarsoft;
 
+import org.custommonkey.xmlunit.Diff;
+import org.custommonkey.xmlunit.NodeTest;
+import org.custommonkey.xmlunit.NodeTester;
+import org.custommonkey.xmlunit.Validator;
+import org.custommonkey.xmlunit.XMLAssert;
+import org.custommonkey.xmlunit.exceptions.ConfigurationException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 import static org.testng.Assert.*;
 
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +24,127 @@ import java.util.List;
  */
 public class AssertUtils {
   private AssertUtils() {
+  }
+  
+
+  public static void assertXMLEqual( Diff diff, boolean assertion ) {
+    XMLAssert.assertXMLEqual( diff, assertion );
+  }
+
+  public static void assertXMLEqual( String msg, Diff diff, boolean assertion ) {
+    XMLAssert.assertXMLEqual( msg, diff, assertion );
+  }
+
+  public static void assertXMLIdentical( Diff diff, boolean assertion ) {
+    XMLAssert.assertXMLIdentical( diff, assertion );
+  }
+
+  public static void assertXMLIdentical( String msg, Diff diff, boolean assertion ) {
+    XMLAssert.assertXMLIdentical( msg, diff, assertion );
+  }
+
+  public static void assertXMLEqual( InputSource test, InputSource control ) throws SAXException, IOException {
+    XMLAssert.assertXMLEqual( test, control );
+  }
+
+  public static void assertXMLEqual( String test, String control ) throws SAXException, IOException {
+    XMLAssert.assertXMLEqual( test, control );
+  }
+
+  public static void assertXMLEqual( Document test, Document control ) {
+    XMLAssert.assertXMLEqual( test, control );
+  }
+
+  public static void assertXMLEqual( Reader test, Reader control ) throws SAXException, IOException {
+    XMLAssert.assertXMLEqual( test, control );
+  }
+
+  public static void assertXMLEqual( String err, InputSource test, InputSource control ) throws SAXException, IOException {
+    XMLAssert.assertXMLEqual( err, test, control );
+  }
+
+  public static void assertXMLEqual( String err, String test, String control ) throws SAXException, IOException {
+    XMLAssert.assertXMLEqual( err, test, control );
+  }
+
+  public static void assertXMLEqual( String err, Document test, Document control ) {
+    XMLAssert.assertXMLEqual( err, test, control );
+  }
+
+  public static void assertXMLEqual( String err, Reader test, Reader control ) throws SAXException, IOException {
+    XMLAssert.assertXMLEqual( err, test, control );
+  }
+
+  public static void assertXMLNotEqual( InputSource test, InputSource control ) throws SAXException, IOException {
+    XMLAssert.assertXMLNotEqual( test, control );
+  }
+
+  public static void assertXMLNotEqual( String test, String control ) throws SAXException, IOException {
+    XMLAssert.assertXMLNotEqual( test, control );
+  }
+
+  public static void assertXMLNotEqual( Document test, Document control ) {
+    XMLAssert.assertXMLNotEqual( test, control );
+  }
+
+  public static void assertXMLNotEqual( Reader test, Reader control ) throws SAXException, IOException {
+    XMLAssert.assertXMLNotEqual( test, control );
+  }
+
+  public static void assertXMLNotEqual( String err, InputSource test, InputSource control ) throws SAXException, IOException {
+    XMLAssert.assertXMLNotEqual( err, test, control );
+  }
+
+  public static void assertXMLNotEqual( String err, String test, String control ) throws SAXException, IOException {
+    XMLAssert.assertXMLNotEqual( err, test, control );
+  }
+
+  public static void assertXMLNotEqual( String err, Document test, Document control ) {
+    XMLAssert.assertXMLNotEqual( err, test, control );
+  }
+
+  public static void assertXMLNotEqual( String err, Reader test, Reader control ) throws SAXException, IOException {
+    XMLAssert.assertXMLNotEqual( err, test, control );
+  }
+
+  public static void assertXMLValid( InputSource xml ) throws SAXException, ConfigurationException {
+    XMLAssert.assertXMLValid( xml );
+  }
+
+  public static void assertXMLValid( String xmlString ) throws SAXException, ConfigurationException {
+    XMLAssert.assertXMLValid( xmlString );
+  }
+
+  public static void assertXMLValid( String xmlString, String systemId ) throws SAXException, ConfigurationException {
+    XMLAssert.assertXMLValid( xmlString, systemId );
+  }
+
+  public static void assertXMLValid( InputSource xml, String systemId ) throws SAXException, ConfigurationException {
+    XMLAssert.assertXMLValid( xml, systemId );
+  }
+
+  public static void assertXMLValid( InputSource xml, String systemId, String doctype ) throws SAXException, ConfigurationException {
+    XMLAssert.assertXMLValid( xml, systemId, doctype );
+  }
+
+  public static void assertXMLValid( String xmlString, String systemId, String doctype ) throws SAXException, ConfigurationException {
+    XMLAssert.assertXMLValid( xmlString, systemId, doctype );
+  }
+
+  public static void assertXMLValid( Validator validator ) {
+    XMLAssert.assertXMLValid( validator );
+  }
+
+  public static void assertNodeTestPasses( NodeTest control, NodeTester tester, short[] nodeTypes, boolean assertion ) {
+    XMLAssert.assertNodeTestPasses( control, tester, nodeTypes, assertion );
+  }
+
+  public static void assertNodeTestPasses( String xmlString, NodeTester tester, short nodeType ) throws SAXException, IOException {
+    XMLAssert.assertNodeTestPasses( xmlString, tester, nodeType );
+  }
+
+  public static void assertNodeTestPasses( InputSource xml, NodeTester tester, short nodeType ) throws SAXException, IOException {
+    XMLAssert.assertNodeTestPasses( xml, tester, nodeType );
   }
 
   public static void assertOne( @Nullable Object current, @NotNull Object... expectedAlternatives ) {
--- test-utils/src/main/java/com/cedarsoft/XmlUnitTest.java
+++ test-utils/src/main/java/com/cedarsoft/XmlUnitTest.java
@@ -24,6 +24,17 @@ public class XmlUnitTest {
                                 "  <extension default=\"true\" delimiter=\".\">cr2</extension>\n" +
                                 "</fileType>"
     );
+    AssertUtils.assertXMLEqual( "<?xml version='1.0' encoding='UTF-8'?>\n" +
+      "<fileType dependent=\"false\">\n" +
+      "  <id>Canon Raw</id>\n" +
+      "  <extension default=\"true\" delimiter=\".\">cr2</extension>\n" +
+      "</fileType>",
+                              "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                                "<fileType dependent=\"false\">\n" +
+                                "  <id>Canon Raw</id>\n" +
+                                "  <extension default=\"true\" delimiter=\".\">cr2</extension>\n" +
+                                "</fileType>"
+    );
   }
 
 }
