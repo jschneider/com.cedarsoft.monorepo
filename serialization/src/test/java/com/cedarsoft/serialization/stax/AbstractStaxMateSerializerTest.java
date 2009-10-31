ab78196c1cbf5f7fb8961b8098e7189d8eef1a48
Johannes Schneider <js@cedarsoft.com>
2009-10-31 13:45:34 +0100

    Serialization: formatted xml output on test

--- serialization/src/test/java/com/cedarsoft/serialization/stax/AbstractStaxMateSerializerTest.java
+++ serialization/src/test/java/com/cedarsoft/serialization/stax/AbstractStaxMateSerializerTest.java
@@ -1,6 +1,12 @@
 package com.cedarsoft.serialization.stax;
 
 import com.cedarsoft.AssertUtils;
+import com.cedarsoft.utils.XmlCommons;
+import org.jdom.Document;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.testng.annotations.*;
@@ -8,6 +14,7 @@ import org.xml.sax.SAXException;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.StringReader;
 
 import static org.testng.Assert.assertEquals;
 
@@ -22,7 +29,7 @@ public abstract class AbstractStaxMateSerializerTest<T> {
     T objectToSerialize = createObjectToSerialize();
 
     byte[] serialized = serializer.serialize( objectToSerialize );
-    AssertUtils.assertXMLEqual( "Was ---\n" + new String( serialized ) + "\n---\nExpected:---\n" + getExpectedSerializedString() + "\n---\n", new String( serialized ), getExpectedSerializedString(), true );
+    AssertUtils.assertXMLEqual( "Was ---\n" + new String( serialized ) + "\n---\nExpected:---\n" + XmlCommons.format( getExpectedSerializedString() ) + "\n---\n", XmlCommons.format( new String( serialized ) ), getExpectedSerializedString(), true );
 
     T deserialized = serializer.deserialize( new ByteArrayInputStream( serialized ) );
 
