21234955e8b57964d9d830866c507c6ddbf6b28a
Johannes Schneider <js@cedarsoft.com>
2009-11-09 21:16:38 +0100

    File: Added file name serializer

---
+++ file/src/main/java/com/cedarsoft/io/FileNameSerializer.java
@@ -0,0 +1,69 @@
+package com.cedarsoft.io;
+
+import com.cedarsoft.FileName;
+import com.cedarsoft.lookup.Lookup;
+import com.cedarsoft.serialization.stax.AbstractStaxMateSerializer;
+import org.codehaus.stax2.XMLStreamReader2;
+import org.codehaus.staxmate.out.SMOutputElement;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+import javax.xml.stream.XMLStreamException;
+import java.io.IOException;
+
+/**
+ * Serializer for file names
+ */
+public class FileNameSerializer extends AbstractStaxMateSerializer<FileName> {
+  @NotNull
+  @NonNls
+  public static final String ELEMENT_EXTENSION = "extension";
+  @NotNull
+  @NonNls
+  public static final String ELEMENT_DELIMITER = "delimiter";
+  @NotNull
+  @NonNls
+  public static final String ELEMENT_BASE_NAME = "baseName";
+
+  public FileNameSerializer() {
+    super( "fileName" );
+  }
+
+  @NotNull
+  @Override
+  public SMOutputElement serialize( @NotNull SMOutputElement serializeTo, @NotNull FileName object, @NotNull Lookup context ) throws IOException, XMLStreamException {
+    serializeTo.addElement( ELEMENT_BASE_NAME ).addCharacters( object.getBaseName().getName() );
+    serializeTo.addElement( ELEMENT_DELIMITER ).addCharacters( object.getDelimiterNonNull() );
+    serializeTo.addElement( ELEMENT_EXTENSION ).addCharacters( object.getExtensionNonNull() );
+    return serializeTo;
+  }
+
+  @NotNull
+  @Override
+  public FileName deserialize( @NotNull XMLStreamReader2 deserializeFrom, @NotNull Lookup context ) throws IOException, XMLStreamException {
+    final String[] delimiter = {null};
+    final String[] base = {null};
+    final String[] extension = {null};
+
+    visitChildren( deserializeFrom, new CB() {
+      @Override
+      public void tagEntered( @NotNull XMLStreamReader2 deserializeFrom, @NotNull String tagName ) throws XMLStreamException, IOException {
+        if ( tagName.equals( ELEMENT_DELIMITER ) ) {
+          delimiter[0] = getText( deserializeFrom );
+        }
+        if ( tagName.equals( ELEMENT_BASE_NAME ) ) {
+          base[0] = getText( deserializeFrom );
+        }
+        if ( tagName.equals( ELEMENT_EXTENSION ) ) {
+          extension[0] = getText( deserializeFrom );
+        }
+      }
+    } );
+
+    if ( delimiter[0] == null ) {
+      return new FileName( base[0], extension[0] );
+    } else {
+      return new FileName( base[0], delimiter[0], extension[0] );
+    }
+  }
+}
---
+++ file/src/test/java/com/cedarsoft/io/FileNameSerializerTest.java
@@ -0,0 +1,38 @@
+package com.cedarsoft.io;
+
+import com.cedarsoft.FileName;
+import com.cedarsoft.serialization.stax.AbstractStaxMateSerializer;
+import com.cedarsoft.serialization.stax.AbstractStaxMateSerializerTest;
+import org.jetbrains.annotations.NotNull;
+
+import static org.testng.Assert.*;
+
+/**
+ *
+ */
+public class FileNameSerializerTest extends AbstractStaxMateSerializerTest<FileName> {
+  @NotNull
+  @Override
+  protected AbstractStaxMateSerializer<FileName> getSerializer() {
+    return new FileNameSerializer();
+  }
+
+  @NotNull
+  @Override
+  protected FileName createObjectToSerialize() {
+    return new FileName( "a", ",", "pdf" );
+  }
+
+  @NotNull
+  @Override
+  protected String getExpectedSerializedString() {
+    return "<fileName><baseName>a</baseName>" +
+      "<delimiter>,</delimiter>" +
+      "<extension>pdf</extension></fileName>";
+  }
+
+  @Override
+  protected void verifyDeserialized( @NotNull FileName fileName ) {
+    assertEquals( fileName, createObjectToSerialize() );
+  }
+}
