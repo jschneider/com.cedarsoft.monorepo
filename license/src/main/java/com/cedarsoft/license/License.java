d948dbd2ddb84e0f62a0538631e2b993700c8e10
Johannes Schneider <js@cedarsoft.com>
2009-11-18 20:42:24 +0100

    Added default licenses

--- license/src/main/java/com/cedarsoft/license/License.java
+++ license/src/main/java/com/cedarsoft/license/License.java
@@ -8,9 +8,15 @@ import org.jetbrains.annotations.NotNull;
  */
 public class License {
   @NotNull
+  public static final License ALL_RIGHTS_RESERVED = new License( "ALL_RIGHTS_RESERVED", "All rights reserved" );
+  @NotNull
+  public static final License PUBLIC_DOMAIN = new License( "PUBLIC_DOMAIN", "Public Domain" );
+
+  @NotNull
   @NonNls
   private final String id;
-  @NotNull  @NonNls
+  @NotNull
+  @NonNls
   private final String name;
 
   public License( @NotNull @NonNls String id, @NotNull @NonNls String name ) {
@@ -27,5 +33,25 @@ public class License {
   @NonNls
   public String getId() {
     return id;
+  }
+
+  @Override
+  public boolean equals( Object o ) {
+    if ( this == o ) return true;
+    if ( !( o instanceof License ) ) return false;
+
+    License license = ( License ) o;
+
+    if ( !id.equals( license.id ) ) return false;
+    if ( !name.equals( license.name ) ) return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id.hashCode();
+    result = 31 * result + name.hashCode();
+    return result;
   }
 }
