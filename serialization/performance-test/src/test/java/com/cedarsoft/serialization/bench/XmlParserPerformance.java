1c4b0f2ddb22cf2ce6bb69c89bdabaf39c1038c1
Johannes Schneider <js@cedarsoft.com>
2009-11-24 18:52:34 +0100

    Serialization: Improved performance test: Added xstream

--- serialization/performance-test/src/test/java/com/cedarsoft/serialization/bench/XmlParserPerformance.java
+++ serialization/performance-test/src/test/java/com/cedarsoft/serialization/bench/XmlParserPerformance.java
@@ -1,10 +1,14 @@
 package com.cedarsoft.serialization.bench;
 
+import com.thoughtworks.xstream.XStream;
 import org.apache.commons.lang.time.StopWatch;
 import org.codehaus.staxmate.SMInputFactory;
+import org.jdom.Document;
+import org.jdom.Element;
 import org.jdom.input.SAXBuilder;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import org.testng.annotations.*;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
@@ -19,6 +23,10 @@ import static org.testng.Assert.*;
  *
  */
 public class XmlParserPerformance {
+  private static final int SMALL = 5000;
+  private static final int MEDIUM = SMALL * 10;
+  private static final int BIG = MEDIUM * 10;
+
   @NotNull
   @NonNls
   public static final String CONTENT_SAMPLE = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
@@ -26,6 +34,36 @@ public class XmlParserPerformance {
     "  <id>Canon Raw</id>\n" +
     "  <extension default=\"true\" delimiter=\".\">cr2</extension>\n" +
     "</fileType>";
+  @NotNull
+  @NonNls
+  public static final String CONTENT_SAMPLE_XSTREAM = "<fileType dependent=\"false\">\n" +
+    "  <id>Canon Raw</id>\n" +
+    "  <extension default=\"true\" delimiter=\".\">\n" +
+    "    <extension>cr2</extension>\n" +
+    "  </extension>\n" +
+    "</fileType>";
+
+
+  @Test
+  public void testCreateXStream() {
+    XStream xStream = createConfiguredXStream();
+
+    FileType fileType = new FileType( "Canon Raw", new Extension( ".", "cr2", true ), false );
+    assertEquals( xStream.toXML( fileType ), CONTENT_SAMPLE_XSTREAM );
+  }
+
+  @NotNull
+  private XStream createConfiguredXStream() {
+    XStream xStream = new XStream();
+    xStream.alias( "fileType", FileType.class );
+    xStream.alias( "extension", Extension.class );
+    xStream.useAttributeFor( FileType.class, "dependent" );
+
+    xStream.useAttributeFor( Extension.class, "delimiter" );
+    xStream.useAttributeFor( Extension.class, "isDefault" );
+    xStream.aliasAttribute( Extension.class, "isDefault", "default" );
+    return xStream;
+  }
 
   //  /*
   //--> 640
@@ -50,14 +88,18 @@ public class XmlParserPerformance {
 
 
   public static void main( String[] args ) {
+    System.out.println();
+    System.out.println( "Xstream (10%)" );
+    new XmlParserPerformance().benchXStream();
+    System.out.println();
     System.out.println( "Jdom (1% of documents):" );
     new XmlParserPerformance().benchJdom();
     System.out.println();
     System.out.println( "Java6:" );
     new XmlParserPerformance().benchJava();
-//    System.out.println();
-//    System.out.println( "Aalto:" );
-//    new XmlParserPerformance().benchAalto();
+//        System.out.println();
+//        System.out.println( "Aalto:" );
+//        new XmlParserPerformance().benchAalto();
     System.out.println();
     System.out.println( "Woodstox:" );
     new XmlParserPerformance().benchWoodstox();
@@ -72,20 +114,37 @@ public class XmlParserPerformance {
     new XmlParserPerformance().benchJavolution();
   }
 
-  /*
---> 597
---> 586
---> 638
---> 500
-  */
+  public void benchXStream() {
+    runBenchmark( new Runnable() {
+      @Override
+      public void run() {
+        try {
+          XStream xStream = createConfiguredXStream();
+          for ( int i = 0; i < MEDIUM; i++ ) {
+            assertNotNull( xStream.fromXML( CONTENT_SAMPLE_XSTREAM ) );
+          }
+        } catch ( Exception e ) {
+          throw new RuntimeException( e );
+        }
+      }
+    }, 4 );
+  }
 
   public void benchJdom() {
     runBenchmark( new Runnable() {
       @Override
       public void run() {
         try {
-          for ( int i = 0; i < 1000; i++ ) {
-            new SAXBuilder().build( new StringReader( CONTENT_SAMPLE ) );
+          for ( int i = 0; i < SMALL; i++ ) {
+            Document doc = new SAXBuilder().build( new StringReader( CONTENT_SAMPLE ) );
+
+            Element fileTypeElement = doc.getRootElement();
+            Element extensionElement = fileTypeElement.getChild( "extension" );
+
+            Extension extension = new Extension( extensionElement.getAttributeValue( "delimiter" ), extensionElement.getText(), extensionElement.getAttribute( "default" ).getBooleanValue() );
+            FileType fileType = new FileType( fileTypeElement.getChildText( "id" ), extension, fileTypeElement.getAttribute( "dependent" ).getBooleanValue() );
+
+            assertNotNull( fileType );
           }
         } catch ( Exception e ) {
           throw new RuntimeException( e );
@@ -165,21 +224,21 @@ public class XmlParserPerformance {
     }, 4 );
   }
 
-//  public void benchAalto() {
-//    runBenchmark( new Runnable() {
-//      @Override
-//      public void run() {
-//        try {
-//          //          XMLInputFactory inputFactory = XMLInputFactory.newInstance( "StAXInputFactory", getClass().getClassLoader() );
-//          XMLInputFactory inputFactory = XMLInputFactory.newInstance( "com.fasterxml.aalto.stax.InputFactoryImpl", getClass().getClassLoader() );
-//
-//          benchParse( inputFactory );
-//        } catch ( Exception e ) {
-//          throw new RuntimeException( e );
-//        }
-//      }
-//    }, 4 );
-//  }
+  //  public void benchAalto() {
+  //    runBenchmark( new Runnable() {
+  //      @Override
+  //      public void run() {
+  //        try {
+  //          //          XMLInputFactory inputFactory = XMLInputFactory.newInstance( "StAXInputFactory", getClass().getClassLoader() );
+  //          XMLInputFactory inputFactory = XMLInputFactory.newInstance( "com.fasterxml.aalto.stax.InputFactoryImpl", getClass().getClassLoader() );
+  //
+  //          benchParse( inputFactory );
+  //        } catch ( Exception e ) {
+  //          throw new RuntimeException( e );
+  //        }
+  //      }
+  //    }, 4 );
+  //  }
 
   public void benchWoodstox() {
     runBenchmark( new Runnable() {
@@ -239,59 +298,75 @@ public class XmlParserPerformance {
   }
 
   private void benchParse( javolution.xml.stream.XMLInputFactory inputFactory ) throws XMLStreamException, javolution.xml.stream.XMLStreamException {
-    for ( int i = 0; i < 100000; i++ ) {
+    for ( int i = 0; i < BIG; i++ ) {
       javolution.xml.stream.XMLStreamReader parser = inputFactory.createXMLStreamReader( new StringReader( CONTENT_SAMPLE ) );
 
 
       assertEquals( parser.nextTag(), XMLStreamReader.START_ELEMENT );
       assertEquals( parser.getLocalName().toString(), "fileType" );
-      assertEquals( parser.getAttributeValue( null, "dependent" ).toString(), "false" );
+
+      boolean dependent = Boolean.parseBoolean( parser.getAttributeValue( null, "dependent" ).toString() );
 
       assertEquals( parser.nextTag(), XMLStreamReader.START_ELEMENT );
       assertEquals( parser.getLocalName().toString(), "id" );
       assertEquals( parser.next(), XMLStreamReader.CHARACTERS );
-      assertEquals( parser.getText().toString(), "Canon Raw" );
+
+      String id = parser.getText().toString();
+
       assertEquals( parser.nextTag(), XMLStreamReader.END_ELEMENT );
       assertEquals( parser.getLocalName().toString(), "id" );
 
       assertEquals( parser.nextTag(), XMLStreamReader.START_ELEMENT );
       assertEquals( parser.getLocalName().toString(), "extension" );
-      assertEquals( parser.getAttributeValue( null, "default" ).toString(), "true" );
-      assertEquals( parser.getAttributeValue( null, "delimiter" ).toString(), "." );
+
+      boolean isDefault = Boolean.parseBoolean( parser.getAttributeValue( null, "default" ).toString() );
+      String delimiter = parser.getAttributeValue( null, "delimiter" ).toString();
+
       assertEquals( parser.next(), XMLStreamReader.CHARACTERS );
-      assertEquals( parser.getText().toString(), "cr2" );
+
+      String extension = parser.getText().toString();
+
       assertEquals( parser.nextTag(), XMLStreamReader.END_ELEMENT );
       assertEquals( parser.getLocalName().toString(), "extension" );
 
       assertEquals( parser.nextTag(), XMLStreamReader.END_ELEMENT );
       assertEquals( parser.getLocalName().toString(), "fileType" );
       assertEquals( parser.next(), XMLStreamReader.END_DOCUMENT );
+
+      parser.close();
+
+      FileType type = new FileType( id, new Extension( delimiter, extension, isDefault ), dependent );
+      assertNotNull( type );
     }
   }
 
   private void benchParse( XMLInputFactory inputFactory ) throws XMLStreamException {
-    for ( int i = 0; i < 100000; i++ ) {
+    for ( int i = 0; i < BIG; i++ ) {
       XMLStreamReader parser = inputFactory.createXMLStreamReader( new StringReader( CONTENT_SAMPLE ) );
-
 
       assertEquals( parser.nextTag(), XMLStreamReader.START_ELEMENT );
       assertEquals( parser.getLocalName(), "fileType" );
       assertEquals( parser.getName().getLocalPart(), "fileType" );
-      assertEquals( parser.getAttributeValue( null, "dependent" ), "false" );
+
+      boolean dependent = Boolean.parseBoolean( parser.getAttributeValue( null, "dependent" ) );
 
       assertEquals( parser.nextTag(), XMLStreamReader.START_ELEMENT );
       assertEquals( parser.getName().getLocalPart(), "id" );
       assertEquals( parser.next(), XMLStreamReader.CHARACTERS );
-      assertEquals( parser.getText(), "Canon Raw" );
+
+      String id = parser.getText();
+
       assertEquals( parser.nextTag(), XMLStreamReader.END_ELEMENT );
-      assertEquals( parser.getName().getLocalPart(), "id" );
 
       assertEquals( parser.nextTag(), XMLStreamReader.START_ELEMENT );
       assertEquals( parser.getName().getLocalPart(), "extension" );
-      assertEquals( parser.getAttributeValue( null, "default" ), "true" );
-      assertEquals( parser.getAttributeValue( null, "delimiter" ), "." );
+
+      boolean isDefault = Boolean.parseBoolean( parser.getAttributeValue( null, "default" ) );
+      String delimiter = parser.getAttributeValue( null, "delimiter" );
+
       assertEquals( parser.next(), XMLStreamReader.CHARACTERS );
-      assertEquals( parser.getText(), "cr2" );
+
+      String extension = parser.getText();
       assertEquals( parser.nextTag(), XMLStreamReader.END_ELEMENT );
       assertEquals( parser.getName().getLocalPart(), "extension" );
 
@@ -299,6 +374,10 @@ public class XmlParserPerformance {
       assertEquals( parser.getName().getLocalPart(), "fileType" );
       assertEquals( parser.next(), XMLStreamReader.END_DOCUMENT );
 
+      parser.close();
+
+      FileType type = new FileType( id, new Extension( delimiter, extension, isDefault ), dependent );
+      assertNotNull( type );
     }
   }
 
@@ -328,4 +407,63 @@ public class XmlParserPerformance {
   }
 
 
+  public static class FileType {
+    private final boolean dependent;
+    @NotNull
+    @NonNls
+    private final String id;
+    @NotNull
+    private final Extension extension;
+
+    public FileType( @NotNull String id, @NotNull Extension extension, boolean dependent ) {
+      this.dependent = dependent;
+      this.id = id;
+      this.extension = extension;
+    }
+
+    @NotNull
+    public Extension getExtension() {
+      return extension;
+    }
+
+    public boolean isDependent() {
+
+      return dependent;
+    }
+
+    @NotNull
+    public String getId() {
+      return id;
+    }
+  }
+
+  public static class Extension {
+    private final boolean isDefault;
+    @NotNull
+    @NonNls
+    private final String delimiter;
+    @NotNull
+    @NonNls
+    private final String extension;
+
+    public Extension( @NotNull String delimiter, @NotNull String extension, boolean aDefault ) {
+      this.delimiter = delimiter;
+      this.extension = extension;
+      isDefault = aDefault;
+    }
+
+    public boolean isDefault() {
+      return isDefault;
+    }
+
+    @NotNull
+    public String getDelimiter() {
+      return delimiter;
+    }
+
+    @NotNull
+    public String getExtension() {
+      return extension;
+    }
+  }
 }
