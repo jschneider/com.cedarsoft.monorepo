74e19e14298fee35adb483ad14f0ce4121ef2622
Johannes Schneider <js@cedarsoft.com>
2009-11-25 19:15:29 +0100

    Deleted db

--- db/genericdao/src/main/java/com/cedarsoft/gdao/InstanceFinder.java
+++
@@ -1,9 +0,0 @@
-package com.cedarsoft.gdao;
-
-/**
- * Returns a single instance.
- *
- * @param <T> the type of the instance this finder returns
- */
-public interface InstanceFinder<T> extends Finder<T> {
-}
