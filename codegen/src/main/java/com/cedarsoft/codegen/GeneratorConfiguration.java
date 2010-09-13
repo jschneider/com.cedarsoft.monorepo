5ac800b1c8de426208ed0b3faaf523c889b75c30
Johannes Schneider <js@cedarsoft.com>
2010-09-13 23:06:18 +0200

    Codegen: Fixed wrong NotNull annotation

--- codegen/src/main/java/com/cedarsoft/codegen/GeneratorConfiguration.java
+++ codegen/src/main/java/com/cedarsoft/codegen/GeneratorConfiguration.java
@@ -76,7 +76,7 @@ public class GeneratorConfiguration {
     this.testResourcesDestination = testResourcesDestination;
   }
 
-  @NotNull
+  @Nullable
   public Classpath getClasspath() {
     return classpath;
   }
