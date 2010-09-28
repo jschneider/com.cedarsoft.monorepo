4abc5a0ebf3a703f379114597a8b8dbb63e33d8a
Johannes Schneider <js@cedarsoft.com>
2010-09-28 11:49:30 +0200

    Added energy/force/intensity units - used for photovoltaric stuff

---
+++ src/main/java/com/cedarsoft/unit/si/J.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (C) cedarsoft GmbH.
+ *
+ * Licensed under the GNU General Public License version 3 (the "License")
+ * with Classpath Exception; you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *         http://www.cedarsoft.org/gpl3ce
+ *         (GPL 3 with Classpath Exception)
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 only, as
+ * published by the Free Software Foundation. cedarsoft GmbH designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by cedarsoft GmbH in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 3 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 3 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact cedarsoft GmbH, 72810 Gomaringen, Germany,
+ * or visit www.cedarsoft.com if you need additional information or
+ * have any questions.
+ */
+
+package com.cedarsoft.unit.si;
+
+import com.cedarsoft.quantity.Energy;
+import com.cedarsoft.quantity.Power;
+import com.cedarsoft.unit.Definition;
+import com.cedarsoft.unit.Name;
+import com.cedarsoft.unit.Symbol;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+/**
+ * Represents a watt
+ *
+ * @noinspection AnnotationNamingConvention
+ */
+@Retention( RetentionPolicy.RUNTIME )
+@Documented
+@Inherited
+
+@Energy
+@Name( "joule" )
+@SiDerivedUnit
+@Symbol( "J" )
+@Definition( {"N*m", "kg*m²/s²", "W*s"} )
+public @interface J {
+}
---
+++ src/main/java/com/cedarsoft/unit/si/N.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C) cedarsoft GmbH.
+ *
+ * Licensed under the GNU General Public License version 3 (the "License")
+ * with Classpath Exception; you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *         http://www.cedarsoft.org/gpl3ce
+ *         (GPL 3 with Classpath Exception)
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 only, as
+ * published by the Free Software Foundation. cedarsoft GmbH designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by cedarsoft GmbH in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 3 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 3 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact cedarsoft GmbH, 72810 Gomaringen, Germany,
+ * or visit www.cedarsoft.com if you need additional information or
+ * have any questions.
+ */
+
+package com.cedarsoft.unit.si;
+
+import com.cedarsoft.quantity.Force;
+import com.cedarsoft.unit.Definition;
+import com.cedarsoft.unit.Name;
+import com.cedarsoft.unit.Symbol;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+/**
+ * Represents a newton
+ *
+ * @noinspection AnnotationNamingConvention
+ */
+@Retention( RetentionPolicy.RUNTIME )
+@Documented
+@Inherited
+
+@Force
+@Name( "newton" )
+@SiDerivedUnit
+@Symbol( "N" )
+@Definition( {"kg*m/s²"} )
+public @interface N {
+}
