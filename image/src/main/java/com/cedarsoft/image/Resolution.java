214c4fe865ea8efa7c7a0691c06f1e58433cc909
Johannes Schneider <js@cedarsoft.com>
2010-10-12 20:35:43 +0200

    updated license headers

--- image/src/main/java/com/cedarsoft/image/Resolution.java
+++ image/src/main/java/com/cedarsoft/image/Resolution.java
@@ -1,15 +1,18 @@
 /**
  * Copyright (C) cedarsoft GmbH.
  *
- * Licensed under the GNU General Public License version 3 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the GNU General Public License version 3 (the "License")
+ * with Classpath Exception; you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
  *
- *         http://www.cedarsoft.org/gpl3
+ *         http://www.cedarsoft.org/gpl3ce
+ *         (GPL 3 with Classpath Exception)
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 3 only, as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation. cedarsoft GmbH designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by cedarsoft GmbH in the LICENSE file that accompanied this code.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
