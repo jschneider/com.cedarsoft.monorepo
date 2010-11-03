ad898abf4451627be6b05d3f2c74448301dd383f
Johannes Schneider <js@cedarsoft.com>
2010-11-03 10:46:11 +0100

    added dpi

---
+++ src/main/java/com/cedarsoft/unit/other/dpi.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) cedarsoft GmbH.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cedarsoft.unit.other;
+
+import com.cedarsoft.quantity.DotDensity;
+import com.cedarsoft.unit.Definition;
+import com.cedarsoft.unit.DerivedUnit;
+import com.cedarsoft.unit.Name;
+import com.cedarsoft.unit.Symbol;
+import com.cedarsoft.unit.si.cm;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+@Retention( RetentionPolicy.RUNTIME )
+@Documented
+@Inherited
+
+@DotDensity
+@Name( "Dots per Inch" )
+@Symbol( "dpi" )
+@Definition( "the number of individual dots that can be placed in a line within the span of 1 inch (2.54 cm)" )
+@DerivedUnit( cm.class )
+public @interface dpi {
+}
