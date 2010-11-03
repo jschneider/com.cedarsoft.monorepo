d90f5ddc2af6cb30718e06eded135bf7f4f7add8
Johannes Schneider <js@cedarsoft.com>
2010-11-03 10:48:57 +0100

    added usage sample

---
+++ src/test/java/com/cedarsoft/UsageSample.java
@@ -0,0 +1,78 @@
+package com.cedarsoft;
+
+import com.cedarsoft.unit.other.dpi;
+import com.cedarsoft.unit.other.px;
+import com.cedarsoft.unit.si.cm;
+import com.cedarsoft.unit.si.m;
+import com.cedarsoft.unit.si.m2;
+import com.cedarsoft.unit.si.mm;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+public class UsageSample {
+  @Test
+  public void testArea1() {
+    @m
+    double length = 7.3;
+    @m
+    double width = 12;
+    @m2
+    double area = calculateArea( length, width );
+
+    assertEquals( 87.6, area, 0 );
+  }
+
+  @Test
+  public void testArea2() {
+    @m
+    double length = 7.3;
+    @cm
+    double width = 1200;
+    @m2
+    double area = calculateAreaSpecial( length, width );
+
+    assertEquals( 87.6, area, 0 );
+  }
+
+  @Test
+  public void testPxToMm() throws Exception {
+    @dpi
+    int res = 72;
+    assertEquals( 10, pixelsToMm( 28, res ), 0.2 );
+  }
+
+  /**
+   * This is a simple method that takes two length arguments of a given unit
+   *
+   * @param length the length
+   * @param width  the width
+   * @return the area
+   */
+  @m2
+  public static double calculateArea( @m double length, @m double width ) {
+    return length * width;
+  }
+
+  /**
+   * This method converts one unit. The conversion has to be done manually.
+   * But this is a strength! Not every unit can be converted losless. Therefore doing it manually if
+   * necessary seems to be the better idea.
+   *
+   * @param length the length
+   * @param width  the width
+   * @return the area
+   */
+  @m2
+  public static double calculateAreaSpecial( @m double length, @cm double width ) {
+    return length * width / 100.0;
+  }
+
+  @mm
+  public static double pixelsToMm( @px int pixels, @dpi double resolution ) {
+    return pixels / resolution * 25.4;
+  }
+}
