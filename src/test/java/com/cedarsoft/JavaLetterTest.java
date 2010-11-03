eaef2d666ebf213c3eb7196a5ac6c074d3886015
Johannes Schneider <js@cedarsoft.com>
2010-11-03 10:36:20 +0100

    renamed test

--- src/test/java/com/cedarsoft/JavaLetterTest.java
+++ src/test/java/com/cedarsoft/JavaLetterTest.java
@@ -23,7 +23,7 @@ import static org.junit.Assert.*;
 /**
  * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
  */
-public class JavaLetter {
+public class JavaLetterTest {
   @Test
   public void testName() throws Exception {
     assertTrue( Character.isJavaIdentifierStart( 'a' ) );
