02ae0846647c805c3ca5bef3b5f2db11d5a40ea8
Johannes Schneider <js@cedarsoft.com>
2010-11-03 10:17:08 +0100

    javadoc

--- src/main/java/com/cedarsoft/unit/Units.java
+++ src/main/java/com/cedarsoft/unit/Units.java
@@ -39,7 +39,7 @@ import org.jetbrains.annotations.NotNull;
 import java.lang.annotation.Annotation;
 
 /**
- * @param <Q> the quantity
+ * Helper class for Units
  * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
  */
 public class Units {
