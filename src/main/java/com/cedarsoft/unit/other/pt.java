1f266da15f82c6c16a96d9c6efbccce28eb163c0
Johannes Schneider <js@cedarsoft.com>
2011-04-09 14:33:01 +0200

    added point

---
+++ src/main/java/com/cedarsoft/unit/other/pt.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) cedarsoft GmbH.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cedarsoft.unit.other;
+
+import com.cedarsoft.quantity.Length;
+import com.cedarsoft.unit.Definition;
+import com.cedarsoft.unit.DerivedUnit;
+import com.cedarsoft.unit.Name;
+import com.cedarsoft.unit.Symbol;
+import com.cedarsoft.unit.si.m;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+@Retention( RetentionPolicy.RUNTIME )
+@Documented
+@Inherited
+
+@Length
+@Symbol( "pt" )
+@Name( "points" )
+@DerivedUnit( {m.class} )
+@Definition( {"72 pt = 1 inch", "=0.3527 mm"} )
+public @interface pt {
+}
