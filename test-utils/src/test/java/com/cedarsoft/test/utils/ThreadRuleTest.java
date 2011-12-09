b24d5226bc71205d2707e09893a40164bf51d4f7
Johannes Schneider <js@cedarsoft.com>
2011-12-09 23:12:00 +0100

    added test for ThreadRule

---
+++ test-utils/src/test/java/com/cedarsoft/test/utils/ThreadRuleTest.java
@@ -0,0 +1,68 @@
+package com.cedarsoft.test.utils;
+
+import org.junit.*;
+
+import static org.fest.assertions.Fail.fail;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+@Ignore
+public class ThreadRuleTest {
+  @Rule
+  public ThreadRule threadRule = new ThreadRule();
+
+  @Test
+  public void testThreadRule() throws Exception {
+    new Thread( new Runnable() {
+      @Override
+      public void run() {
+        try {
+          Thread.sleep( 100 );
+        } catch ( InterruptedException e ) {
+          throw new RuntimeException( e );
+        }
+      }
+    } ).start();
+  }
+
+  @Test
+  public void testMulti() throws Exception {
+    new Thread( new Runnable() {
+      @Override
+      public void run() {
+        try {
+          Thread.sleep( 100 );
+        } catch ( InterruptedException e ) {
+          throw new RuntimeException( e );
+        }
+      }
+    } ).start();
+    new Thread( new Runnable() {
+      @Override
+      public void run() {
+        try {
+          Thread.sleep( 100 );
+        } catch ( InterruptedException e ) {
+          throw new RuntimeException( e );
+        }
+      }
+    } ).start();
+  }
+
+  @Test
+  public void testFailing() throws Exception {
+    new Thread( new Runnable() {
+      @Override
+      public void run() {
+        try {
+          Thread.sleep( 100 );
+        } catch ( InterruptedException e ) {
+          throw new RuntimeException( e );
+        }
+      }
+    } ).start();
+
+    fail( "failing..." );
+  }
+}
