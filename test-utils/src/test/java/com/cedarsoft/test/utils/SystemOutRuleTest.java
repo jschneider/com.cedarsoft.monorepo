16ef52a33c8ce08bedaf80f863527f2a1e6e96fe
Johannes Schneider <js@cedarsoft.com>
2012-01-17 16:40:08 +0100

    fixed line separator

--- test-utils/src/test/java/com/cedarsoft/test/utils/SystemOutRuleTest.java
+++ test-utils/src/test/java/com/cedarsoft/test/utils/SystemOutRuleTest.java
@@ -41,30 +41,36 @@ import static org.junit.Assert.*;
 public class SystemOutRuleTest {
   @Rule
   public SystemOutRule rule = new SystemOutRule();
+  private String n;
+
+  @Before
+  public void setUp() throws Exception {
+    n = System.getProperty("line.separator");
+  }
 
   @Test
   public void testIt() {
-    System.out.println( "Hey" );
-    System.out.println( "2" );
-    assertEquals( "Hey\n2\n", rule.getOutAsString() );
-    System.out.println( "3" );
-    assertEquals( "Hey\n2\n3\n", rule.getOutAsString() );
+    System.out.println("Hey");
+    System.out.println("2");
+    assertEquals("Hey" + n + "2" + n + "", rule.getOutAsString());
+    System.out.println("3");
+    assertEquals("Hey" + n + "2" + n + "3" + n + "", rule.getOutAsString());
   }
 
   @Test
   public void testOut2() {
-    System.out.println( "2" );
-    assertEquals( "2\n", rule.getOutAsString() );
-    System.out.println( "3" );
-    assertEquals( "2\n3\n", rule.getOutAsString() );
+    System.out.println("2");
+    assertEquals("2" + n + "", rule.getOutAsString());
+    System.out.println("3");
+    assertEquals("2" + n + "3" + n + "", rule.getOutAsString());
   }
 
   @Test
   public void testErr() {
-    System.err.println( "Hey" );
-    System.err.println( "2" );
-    assertEquals( "Hey\n2\n", rule.getErrAsString() );
-    System.err.println( "3" );
-    assertEquals( "Hey\n2\n3\n", rule.getErrAsString() );
+    System.err.println("Hey");
+    System.err.println("2");
+    assertEquals("Hey" + n + "2" + n + "", rule.getErrAsString());
+    System.err.println("3");
+    assertEquals("Hey" + n + "2" + n + "3" + n + "", rule.getErrAsString());
   }
 }
