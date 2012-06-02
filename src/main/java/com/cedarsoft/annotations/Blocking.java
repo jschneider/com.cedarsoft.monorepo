f2eca260d16ebb1ec539998013e878be9e33058f
Johannes Schneider <js@cedarsoft.com>
2012-06-02 20:30:51 +0200

    added blocking/non blocking annotations

---
+++ src/main/java/com/cedarsoft/annotations/Blocking.java
@@ -0,0 +1,16 @@
+package com.cedarsoft.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+@Documented
+@Retention( RetentionPolicy.RUNTIME )
+@Target( {ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD} )
+public @interface Blocking {
+}
