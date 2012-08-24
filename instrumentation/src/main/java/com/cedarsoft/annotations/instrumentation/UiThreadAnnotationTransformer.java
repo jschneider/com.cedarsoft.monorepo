e4d6dd5c15ad09303f4acf978bd14993f7e9d86a
Johannes Schneider <js@cedarsoft.com>
2012-08-25 00:11:15 +0200

    inlined abstract base class

--- instrumentation/src/main/java/com/cedarsoft/annotations/instrumentation/UiThreadAnnotationTransformer.java
+++ instrumentation/src/main/java/com/cedarsoft/annotations/instrumentation/UiThreadAnnotationTransformer.java
@@ -1,19 +1,110 @@
 package com.cedarsoft.annotations.instrumentation;
 
+import com.cedarsoft.annotations.NonUiThread;
+import com.cedarsoft.annotations.UiThread;
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.LoaderClassPath;
+import javassist.NotFoundException;
+
 import javax.annotation.Nonnull;
+import java.lang.annotation.Annotation;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
 
 /**
  * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
  */
-public class UiThreadAnnotationTransformer extends AbstractUiThreadAnnotationTransformer {
+public class UiThreadAnnotationTransformer implements ClassFileTransformer {
   @Nonnull
-  public static final String VERIFY_UI_THREAD = "com.cedarsoft.annotations.verification.VerifyUiThread.verifyUiThreadAsserted();";
-//  public static final String VERIFY_UI_THREAD = "assert (com.cedarsoft.annotations.verification.VerifyUiThread.verifyUiThreadAsserted());";
+  private static CtClass getCtClass( @Nonnull final ClassLoader loader, @Nonnull String className ) throws NotFoundException {
+    final ClassPool pool = ClassPool.getDefault();
+    final LoaderClassPath lcp = new LoaderClassPath( loader );
+    pool.appendClassPath( lcp );
+
+    return pool.get( className );
+  }
+
+  private static boolean isAnnotated( @Nonnull CtMethod method, @Nonnull Class<? extends Annotation> annotationType ) throws ClassNotFoundException {
+    for ( Object annotation : method.getAnnotations() ) {
+      System.out.println( "Annotation: " + annotation );
+      System.out.println( "Annotation: " + annotation.getClass().getName() );
+
+      if ( annotationType.isAssignableFrom( annotation.getClass() ) ) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  @Override
+  public byte[] transform( ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer ) throws IllegalClassFormatException {
+    System.out.println( "---------------------" );
+    System.out.println( "UiThreadAnnotationTransformer.transform: " + classBeingRedefined.getName() );
+
+    try {
+      final CtClass ctClass = UiThreadAnnotationTransformer.getCtClass( loader, classBeingRedefined.getName() );
+      ctClass.toBytecode();
+
+      transformClass( ctClass );
+      return ctClass.toBytecode();
+
+    } catch ( Exception e ) {
+      throw new RuntimeException( e );
+    }
+  }
+
+  private void transformClass( @Nonnull CtClass ctClass ) throws ClassNotFoundException, CannotCompileException, NotFoundException {
+    for ( CtMethod method : ctClass.getMethods() ) {
+      boolean uiThread = isAnnotated( method, UiThread.class );
+      boolean nonUiThread = isAnnotated( method, NonUiThread.class );
+
+      if ( uiThread && nonUiThread ) {
+        throw new IllegalStateException( "Must not have both annotations in " + ctClass.getName() + "#" + method.getName() + ": " + UiThread.class.getName() + " and " + NonUiThread.class.getName() );
+      }
+
+      if ( uiThread ) {
+        ctClass.defrost();
+        insertUiThreadVerification( method );
+      }
+      if ( nonUiThread ) {
+        ctClass.defrost();
+        insertNonUiThreadVerification( method );
+      }
+    }
+  }
+
+  private static void insertUiThreadVerification( @Nonnull CtMethod methodOld ) throws CannotCompileException {
+    StringBuilder body = new StringBuilder();
+
+    body.append( "{" );
+    body.append( appendUiThreadVerificationCode() );
+    body.append( "}" );
+
+    methodOld.insertBefore( body.toString() );
+  }
+
+  private static void insertNonUiThreadVerification( @Nonnull CtMethod methodOld ) throws CannotCompileException {
+    StringBuilder body = new StringBuilder();
+
+    body.append( "{" );
+    body.append( appendNonUiThreadVerificationCode() );
+    body.append( "}" );
+
+    methodOld.insertBefore( body.toString() );
+  }
 
   @Nonnull
-  @Override
-  protected String appendVerificationCode() {
-    return VERIFY_UI_THREAD;
-//    return "assert( true);";
+  private static String appendUiThreadVerificationCode() {
+    return "com.cedarsoft.annotations.verification.VerifyUiThread.verifyUiThreadAsserted();";
+  }
+
+  @Nonnull
+  private static String appendNonUiThreadVerificationCode() {
+    return "com.cedarsoft.annotations.verification.VerifyUiThread.verifyNonUiThreadAsserted();";
   }
 }
