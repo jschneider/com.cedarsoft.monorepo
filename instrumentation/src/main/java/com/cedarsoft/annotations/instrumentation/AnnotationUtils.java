0e0125bbfe4dbc6b95ced5360c1d05d813dc3e13
Johannes Schneider <js@cedarsoft.com>
2013-11-13 22:48:25 +0100

    javadoc

--- instrumentation/src/main/java/com/cedarsoft/annotations/instrumentation/AnnotationUtils.java
+++ instrumentation/src/main/java/com/cedarsoft/annotations/instrumentation/AnnotationUtils.java
@@ -13,6 +13,12 @@ public class AnnotationUtils {
   private AnnotationUtils() {
   }
 
+  /**
+   * Returns whether the given annotations contains the searched annotation
+   * @param annotations a array of annotations
+   * @param annotationType the annotation type
+   * @return true if an annotation of the given type is contained, false otherwise
+   */
   public static boolean hasAnnotation( @Nonnull Object[] annotations, @Nonnull Class<? extends Annotation> annotationType ) {
     for ( Object annotation : annotations ) {
       if ( isOfType( ( Annotation ) annotation, annotationType ) ) {
@@ -22,12 +28,25 @@ public class AnnotationUtils {
     return false;
   }
 
+  /**
+   * Returns whether the given annotation is of the queried type
+   * @param annotation the annotation
+   * @param expectedAnnotationType the exprected annotation type
+   * @return true if the annotation is of the given type, false otherwise
+   */
   public static boolean isOfType( @Nonnull Annotation annotation, @Nonnull Class<? extends Annotation> expectedAnnotationType ) {
     //if ( annotation.annotationType().getName().equals( expectedAnnotationType.getName() ) ) {
     return annotation.annotationType().equals( expectedAnnotationType );
   }
 
 
+  /**
+   * Returns the (first) annotation of the given type, or null
+   * @param type the type
+   * @param annotationType the annotation type
+   * @param <T> the annotation type
+   * @return the annotation - if found - or null
+   */
   @Nullable
   public static <T extends Annotation> T findAnnotation( @Nonnull Class<?> type, @Nonnull Class<T> annotationType ) {
     for ( Annotation annotation : type.getAnnotations() ) {
