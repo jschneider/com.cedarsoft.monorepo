fc9bb2c88563934944243a37cc77fab34a76bed3
Johannes Schneider <js@cedarsoft.com>
2015-07-03 19:49:04 +0200

    added XdgUtil

---
+++ app/src/test/java/com/cedarsoft/app/xdg/XdgUtilDemo.java
@@ -0,0 +1,23 @@
+package com.cedarsoft.app.xdg;
+
+import com.google.common.base.StandardSystemProperty;
+import org.junit.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+public class XdgUtilDemo {
+  @Test
+  public void testIt() throws Exception {
+    String value = StandardSystemProperty.OS_NAME.value();
+    if ( !value.contains( "Linux" ) ) {
+      System.err.println( "Not running on Linux!" );
+      return;
+    }
+
+    System.err.println( "Cache Home: " + XdgUtil.getCacheHome().getAbsolutePath() );
+    System.err.println( "Data Home: " + XdgUtil.getDataHome().getAbsolutePath() );
+  }
+}
