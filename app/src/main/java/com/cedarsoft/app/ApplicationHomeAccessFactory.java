f896466054ac9a4de26daa3195090d75863b9279
Johannes Schneider <js@cedarsoft.com>
2015-07-03 19:59:40 +0200

    creating home access for linux

---
+++ app/src/main/java/com/cedarsoft/app/ApplicationHomeAccessFactory.java
@@ -0,0 +1,39 @@
+package com.cedarsoft.app;
+
+import com.cedarsoft.app.xdg.XdgUtil;
+
+import javax.annotation.Nonnull;
+import java.io.File;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+public class ApplicationHomeAccessFactory {
+  @Nonnull
+  public static ApplicationHomeAccess create( @Nonnull String applicationName ) {
+    String osName = System.getProperty( "os.name" );
+    if ( osName == null ) {
+      throw new IllegalStateException( "Property os.name not found" );
+    }
+
+    if ( osName.contains( "Linux" ) ) {
+      return createLinuxHomeAccess( applicationName );
+    }
+
+    if ( osName.contains( "Windows" ) ) {
+      return createWindowsHomeAccess( applicationName );
+    }
+
+    throw new IllegalStateException( "Unsupported OS: " + osName );
+  }
+
+  @Nonnull
+  private static ApplicationHomeAccess createWindowsHomeAccess( @Nonnull String applicationName ) {
+    throw new UnsupportedOperationException( "Not yet implemented" );
+  }
+
+  @Nonnull
+  private static ApplicationHomeAccess createLinuxHomeAccess( @Nonnull String applicationName ) {
+    return new StaticApplicationHomeAccess( applicationName, new File( XdgUtil.getConfigHome(), applicationName ), new File( XdgUtil.getDataHome(), applicationName ), new File( XdgUtil.getCacheHome(), applicationName ) );
+  }
+}
