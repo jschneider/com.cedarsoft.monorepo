a045961d17ef9afced4e268efd4ffbf356e741e7
Johannes Schneider <js@cedarsoft.com>
2016-08-25 21:39:47 +0200

    add FileOutputStreamWithMove

---
+++ io/src/main/java/com/cedarsoft/io/FileOutputStreamWithMove.java
@@ -0,0 +1,53 @@
+package com.cedarsoft.io;
+
+import javax.annotation.Nonnull;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+public class FileOutputStreamWithMove extends FilterOutputStream {
+  public static final String SUFFIX_TMP = ".tmp";
+
+  private boolean closed;
+  @Nonnull
+  private final File tmpFile;
+  @Nonnull
+  private final File file;
+
+  public FileOutputStreamWithMove(@Nonnull File file) throws FileNotFoundException {
+    //noinspection ConstantConditions
+    super(null);
+
+    tmpFile = new File(file.getParent(), file.getName() + SUFFIX_TMP);
+    this.file = file;
+    tmpFile.deleteOnExit();
+
+    this.out = new BufferedOutputStream(new FileOutputStream(tmpFile));
+  }
+
+  @Override
+  public void close() throws IOException {
+    super.close();
+    if (closed) {
+      return;
+    }
+    //Only move the file if it exists
+    try {
+      if (tmpFile.exists()) {
+        Files.move(tmpFile.toPath(), file.toPath());
+      }
+    } catch (IOException e) {
+      //Do not throw the io exception to avoid "Self-suppression not permitted"
+      Thread.currentThread().getUncaughtExceptionHandler().uncaughtException(Thread.currentThread(), e);
+    }
+    closed = true;
+  }
+}
+
---
+++ io/src/test/java/com/cedarsoft/io/FileOutputStreamWithMoveTest.java
@@ -0,0 +1,36 @@
+package com.cedarsoft.io;
+
+import org.junit.*;
+import org.junit.rules.*;
+
+import java.io.File;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+public class FileOutputStreamWithMoveTest {
+  @Rule
+  public TemporaryFolder tmp = new TemporaryFolder();
+
+  @Test
+  public void basics() throws Exception {
+    File targetOut = new File(tmp.newFolder(), "asdf");
+    assertThat(targetOut).doesNotExist();
+
+    FileOutputStreamWithMove out = new FileOutputStreamWithMove(targetOut);
+    assertThat(targetOut).doesNotExist();
+
+    out.write("asdf".getBytes());
+    assertThat(targetOut).doesNotExist();
+
+    out.close();
+    assertThat(targetOut).exists();
+
+    out.close();
+    out.close();
+    out.close();
+    out.close();
+  }
+}
