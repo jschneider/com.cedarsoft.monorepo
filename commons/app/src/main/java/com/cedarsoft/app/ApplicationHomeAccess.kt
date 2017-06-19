92d1dcfe338f77b91b8aeffec5d729e94ea02781
Johannes Schneider <js@cedarsoft.com>
2017-06-19 19:53:15 +0200

    convert to kotlin

--- commons/app/src/main/java/com/cedarsoft/app/ApplicationHomeAccess.kt
+++ commons/app/src/main/java/com/cedarsoft/app/ApplicationHomeAccess.kt
@@ -1,71 +1,61 @@
 /**
  * Copyright (C) cedarsoft GmbH.
- *
+
  * Licensed under the GNU General Public License version 3 (the "License")
  * with Classpath Exception; you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- *
- *         http://www.cedarsoft.org/gpl3ce
- *         (GPL 3 with Classpath Exception)
- *
+
+ * http://www.cedarsoft.org/gpl3ce
+ * (GPL 3 with Classpath Exception)
+
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 3 only, as
  * published by the Free Software Foundation. cedarsoft GmbH designates this
  * particular file as subject to the "Classpath" exception as provided
  * by cedarsoft GmbH in the LICENSE file that accompanied this code.
- *
+
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * version 3 for more details (a copy is included in the LICENSE file that
  * accompanied this code).
- *
+
  * You should have received a copy of the GNU General Public License version
  * 3 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
+
  * Please contact cedarsoft GmbH, 72810 Gomaringen, Germany,
  * or visit www.cedarsoft.com if you need additional information or
  * have any questions.
  */
 
-package com.cedarsoft.app;
+package com.cedarsoft.app
 
-import javax.annotation.Nonnull;
-
-import java.io.File;
+import java.io.File
 
 /**
  * Offers access to the application home dir.
- *
- * @author Johannes Schneider (<a href=mailto:js@cedarsoft.com>js@cedarsoft.com</a>)
+
+ * @author Johannes Schneider ([js@cedarsoft.com](mailto:js@cedarsoft.com))
  */
-public interface ApplicationHomeAccess {
+interface ApplicationHomeAccess {
   /**
    * Returns the application name
-   *
-   * @return the application name
    */
-  @Nonnull
-  String getApplicationName();
-
+  val applicationName: String
 
   /**
    * Returns the directory that should be used for the configuration
    */
-  @Nonnull
-  File getConfigHome();
+  val configHome: File
 
   /**
    * Returns the directory that should be used for the user specific data
    */
-  @Nonnull
-  File getDataHome();
+  val dataHome: File
 
   /**
    * Returns the directory that should be used for caches
    */
-  @Nonnull
-  File getCacheHome();
-
+  val cacheHome: File
 }
