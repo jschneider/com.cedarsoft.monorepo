18e43e466d35eaf6701c18b2e850b367a95cbe4d
Johannes Schneider <js@cedarsoft.com>
2017-06-28 11:27:52 +0200

    add spring boot sample app

---
+++ tests/spring/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+  @Test
+  public void contextLoads() {
+  }
+}
