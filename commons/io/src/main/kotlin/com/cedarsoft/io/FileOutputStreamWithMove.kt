420b54f554a9afa3067ef7b2455a31a29968f121
Johannes Schneider <js@cedarsoft.com>
2017-07-10 16:21:24 +0200

    convert to kotlin

--- commons/io/src/main/kotlin/com/cedarsoft/io/FileOutputStreamWithMove.kt
+++ commons/io/src/main/kotlin/com/cedarsoft/io/FileOutputStreamWithMove.kt
@@ -1,46 +1,58 @@
 package com.cedarsoft.io
 
-import java.io.*
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FilterOutputStream
+import java.io.IOException
 import java.nio.file.Files
 
 /**
+ * Writes to a temporary file and moves to the target file name on [close]
  * @author Johannes Schneider ([js@cedarsoft.com](mailto:js@cedarsoft.com))
  */
 class FileOutputStreamWithMove @Throws(FileNotFoundException::class)
-
 constructor(private val file: File) : FilterOutputStream(null) {
+  /**
+   * Whether the stream has been closed already
+   */
+  private var closed: Boolean = false
+  /**
+   * The tmp file that is written first
+   */
+  private val tmpFile: File
 
-    private var closed: Boolean = false
-    private val tmpFile: File
+  init {
+    tmpFile = File(file.parent, file.name + SUFFIX_TMP)
+    tmpFile.deleteOnExit()
 
-    init {
+    this.out = tmpFile.outputStream().buffered()
+  }
 
-        tmpFile = File(file.parent, file.name + SUFFIX_TMP)
-        tmpFile.deleteOnExit()
+  @Throws(IOException::class)
+  override fun close() {
+    super.close()
 
-        this.out = BufferedOutputStream(FileOutputStream(tmpFile))
+    if (closed) {
+      return
     }
 
-    @Throws(IOException::class)
-    override fun close() {
-        super.close()
-        if (closed) {
-            return
-        }
-        //Only move the file if it exists
-        if (tmpFile.exists()) {
-            //delete the original file first - overwrite mode
-            if (file.exists()) {
-                file.delete()
-            }
+    //Only move the file if it exists
+    if (tmpFile.exists()) {
+      //delete the original file first - overwrite mode
+      if (file.exists()) {
+        file.delete()
+      }
 
-            Files.move(tmpFile.toPath(), file.toPath())
-        }
-        closed = true
+      Files.move(tmpFile.toPath(), file.toPath())
     }
+    closed = true
+  }
 
-    companion object {
-        val SUFFIX_TMP = ".tmp"
-    }
+  companion object {
+    /**
+     * The suffix for the tmp file
+     */
+    val SUFFIX_TMP = ".tmp"
+  }
 }
 
