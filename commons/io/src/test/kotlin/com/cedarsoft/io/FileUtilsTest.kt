5c393934e0dd3a00c1f9e2c35e225ec4528d6e2e
Johannes Schneider <js@cedarsoft.com>
2017-07-10 17:14:53 +0200

    add test

--- commons/io/src/test/kotlin/com/cedarsoft/io/FileUtilsTest.kt
+++ commons/io/src/test/kotlin/com/cedarsoft/io/FileUtilsTest.kt
@@ -1,23 +1,19 @@
 package com.cedarsoft.io
 
-import org.assertj.core.api.Assertions
-import org.junit.Rule
+import com.cedarsoft.test.utils.TempFolder
+import com.cedarsoft.test.utils.WithTempFiles
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.rules.TemporaryFolder
+import java.io.File
 
 /**
  * @author Johannes Schneider ([js@cedarsoft.com](mailto:js@cedarsoft.com))
  */
+@WithTempFiles
 internal class FileUtilsTest {
   @Test
-  internal fun testInlineFunction() {
-    val folder = FileUtilsTest.companion.tmp.newFolder()
-    Assertions.assertThat(folder).isFile().exists()
-  }
-
-  object companion {
-    @JvmStatic
-    @Rule
-    var tmp = TemporaryFolder()
+  internal fun testInlineFunction(@TempFolder folder: File) {
+    assertThat(folder).isDirectory().exists()
+    assertThat(folder.createDirectoryIfNotExisting()).isSameAs(folder)
   }
 }
