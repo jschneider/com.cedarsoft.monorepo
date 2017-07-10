366a5e4f48f16017ab30b475f5f985aed10e9f36
Johannes Schneider <js@cedarsoft.com>
2017-07-10 13:21:12 +0200

    convert to kotlin

--- serialization/serialization/src/main/java/com/cedarsoft/serialization/AbstractXmlSerializer.kt
+++ serialization/serialization/src/main/java/com/cedarsoft/serialization/AbstractXmlSerializer.kt
@@ -1,81 +1,57 @@
 /**
  * Copyright (C) cedarsoft GmbH.
- *
+
  * Licensed under the GNU General Public License version 3 (the "License")
  * with Classpath Exception; you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- *
- *         http://www.cedarsoft.org/gpl3ce
- *         (GPL 3 with Classpath Exception)
- *
+
+ * http://www.cedarsoft.org/gpl3ce
+ * (GPL 3 with Classpath Exception)
+
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 3 only, as
  * published by the Free Software Foundation. cedarsoft GmbH designates this
  * particular file as subject to the "Classpath" exception as provided
  * by cedarsoft GmbH in the LICENSE file that accompanied this code.
- *
+
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * version 3 for more details (a copy is included in the LICENSE file that
  * accompanied this code).
- *
+
  * You should have received a copy of the GNU General Public License version
  * 3 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
+
  * Please contact cedarsoft GmbH, 72810 Gomaringen, Germany,
  * or visit www.cedarsoft.com if you need additional information or
  * have any questions.
  */
 
-package com.cedarsoft.serialization;
+package com.cedarsoft.serialization
 
-import com.cedarsoft.version.VersionRange;
-
-import javax.annotation.Nonnull;
+import com.cedarsoft.version.VersionRange
 
 /**
  * Abstract base class for xml based serializers.
  * The version information is stored within the namespace declaration of the root element
- *
  * @param <T> the type of object this serializer is able to (de)serialize
  * @param <S> the object to serialize to
  * @param <D> the object to deserialize from
  * @param <E> the exception that might be thrown
  */
-public abstract class AbstractXmlSerializer<T, S, D, E extends Throwable> extends AbstractNameSpaceBasedSerializer<T, S, D, E> {
-  @Nonnull
+abstract class AbstractXmlSerializer<T, S, D, E : Throwable>
+/**
+ * Creates a new serializer
+ * @param defaultElementName the default element name
+ * @param nameSpaceBase      the base for the namespace uri
+ * @param formatVersionRange the version range. The max value is used when written.
+ */
+protected constructor(
+  val defaultElementName: String, nameSpaceBase: String, formatVersionRange: VersionRange) : AbstractNameSpaceBasedSerializer<T, S, D, E>(nameSpaceBase, formatVersionRange) {
 
-  private final String defaultElementName;
-
-  /**
-   * Creates a new serializer
-   *
-   * @param defaultElementName the default element name
-   * @param nameSpaceBase      the base for the namespace uri
-   * @param formatVersionRange the version range. The max value is used when written.
-   */
-  protected AbstractXmlSerializer( @Nonnull String defaultElementName, @Nonnull String nameSpaceBase, @Nonnull VersionRange formatVersionRange ) {
-    super( nameSpaceBase, formatVersionRange );
-    this.defaultElementName = defaultElementName;
+  override fun <ST> getSerializer(type: Class<ST>): AbstractXmlSerializer<in ST, S, D, E> {
+    return super.getSerializer(type) as AbstractXmlSerializer<in ST, S, D, E>
   }
-
-  /**
-   * Returns the default element name
-   *
-   * @return the default element name
-   */
-  @Nonnull
-
-  public String getDefaultElementName() {
-    return defaultElementName;
-  }
-
-  @Nonnull
-  @Override
-  public <ST> AbstractXmlSerializer<? super ST, S, D, E> getSerializer(@Nonnull Class<ST> type ) {
-    return ( AbstractXmlSerializer<? super ST, S, D, E> ) super.getSerializer(type );
-  }
-
 }
