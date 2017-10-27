438e713dcd29f1ed95a74103bf65b7a0d94255f0
Johannes Schneider <js@cedarsoft.com>
2017-10-27 16:57:49 +0200

    add #getStageSafe

--- commons/javafx/src/main/java/com/cedarsoft/commons/javafx/FxUtils.kt
+++ commons/javafx/src/main/java/com/cedarsoft/commons/javafx/FxUtils.kt
@@ -1,8 +1,10 @@
 package com.cedarsoft.commons.javafx
 
 import com.google.common.base.Strings
+import com.sun.javafx.stage.StageHelper
 import javafx.scene.Node
 import javafx.scene.Parent
+import javafx.stage.Stage
 import java.io.PrintStream
 
 /**
@@ -17,5 +19,26 @@ object FxUtils {
   private fun dump(node: Node, out: PrintStream, depth: Int) {
     out.println(Strings.repeat("  ", depth) + node + " #" + node.id)
     (node as? Parent)?.childrenUnmodifiable?.forEach { c -> dump(c, out, depth + 1) }
+  }
+
+  /**
+   * Returns the currently focused stage, or the one open stage or null if there is no stage or there are multiple stages
+   */
+  fun getStageSafe(): Stage? {
+    val stages = StageHelper.getStages()
+    for (stage in stages) {
+      if (stage.isFocused) {
+        return stage;
+      }
+    }
+
+    //we have no focused stage, therefore return the one stage if there is only one
+
+    if (stages.size == 1) {
+      return stages.get(0)
+    }
+
+    //no stage found
+    return null;
   }
 }
