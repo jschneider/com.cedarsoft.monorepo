54b4207049dcde8228d140a4d49e071116dcb572
Johannes Schneider <js@cedarsoft.com>
2018-05-24 22:33:55 +0200

    doc

--- commons/test-utils/src/main/java/com/cedarsoft/test/utils/WithTempFiles.kt
+++ commons/test-utils/src/main/java/com/cedarsoft/test/utils/WithTempFiles.kt
@@ -3,6 +3,7 @@ package com.cedarsoft.test.utils
 import org.junit.jupiter.api.extension.ExtendWith
 
 /**
+ * Use {@link WithTempFiles} at the class/method and add {@link TempFolder} oder {@link TempFile} to the test method parameters
  * @author Johannes Schneider ([js@cedarsoft.com](mailto:js@cedarsoft.com))
  */
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FILE, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
