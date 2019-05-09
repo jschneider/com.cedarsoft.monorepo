aaaa5cffd7f1d46e9632a4aa98491f78603b7090
Robot <robot@cedarsoft.com>
2019-05-09 02:28:39 +0000

    publishing updated sources

--- commons/javafx/src/main/java/com/cedarsoft/commons/javafx/BindingsHelper.java
+++ commons/javafx/src/main/java/com/cedarsoft/commons/javafx/BindingsHelper.java
@@ -11,13 +11,13 @@ import javafx.beans.binding.Binding;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.LongBinding;
 import javafx.beans.binding.StringBinding;
+import javafx.beans.binding.StringExpression;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.Property;
 import javafx.beans.property.ReadOnlyIntegerProperty;
 import javafx.beans.property.ReadOnlyProperty;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableBooleanValue;
 import javafx.beans.value.ObservableValue;
@@ -64,7 +64,7 @@ public class BindingsHelper {
 
 
   @Nonnull
-  public static StringBinding toBinding(@Nonnull StringProperty stringProperty) {
+  public static StringBinding toBinding(@Nonnull StringExpression stringProperty) {
     return Bindings.createStringBinding(stringProperty::get, stringProperty);
   }
 
