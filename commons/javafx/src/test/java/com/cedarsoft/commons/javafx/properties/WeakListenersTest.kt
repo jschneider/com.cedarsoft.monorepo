3e04cf5cfb29f67392644b0bd965dbb340c3f095
Robot <robot@cedarsoft.com>
2019-10-09 02:25:54 +0000

    publishing updated sources

---
+++ commons/javafx/src/test/java/com/cedarsoft/commons/javafx/properties/WeakListenersTest.kt
@@ -0,0 +1,47 @@
+package com.cedarsoft.commons.javafx.properties
+
+import com.cedarsoft.commons.javafx.consume
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.lang.System.gc
+import java.util.concurrent.atomic.AtomicInteger
+
+/**
+ * @author Johannes Schneider ([js@cedarsoft.com](mailto:js@cedarsoft.com))
+ */
+class WeakListenersTest {
+  var foo: Foo = Foo()
+
+  @BeforeEach
+  internal fun setUp() {
+    foo = Foo()
+  }
+
+  @Test
+  internal fun testConsume() {
+    assertThat(foo.name).isEqualTo("")
+
+    val counter = AtomicInteger()
+
+    assertThat(counter.get()).isEqualTo(0)
+    registerListener(counter)
+
+    assertThat(counter.get()).isEqualTo(1)
+    foo.name = "asdf17"
+    assertThat(counter.get()).isEqualTo(2)
+
+    for (i in 1..10) {
+      gc()
+    }
+
+    foo.name = "asdf2"
+    assertThat(counter.get()).isEqualTo(3)
+  }
+
+  private fun registerListener(counter: AtomicInteger) {
+    foo.nameProperty.consume {
+      counter.incrementAndGet()
+    }
+  }
+}
