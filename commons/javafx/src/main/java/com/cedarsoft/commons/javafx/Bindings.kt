eb3d775a5c7f43f368979d2cd6ba6e4e070ad07b
Robot <robot@cedarsoft.com>
2021-08-31 03:34:04 +0000

    publishing updated sources

--- commons/javafx/src/main/java/com/cedarsoft/commons/javafx/Bindings.kt
+++ commons/javafx/src/main/java/com/cedarsoft/commons/javafx/Bindings.kt
@@ -1,5 +1,6 @@
 package com.cedarsoft.commons.javafx
 
+import com.cedarsoft.dispose.Disposable
 import javafx.beans.binding.Bindings
 import javafx.beans.binding.StringBinding
 import javafx.beans.property.StringProperty
@@ -38,21 +39,29 @@ fun <T> ObservableList<T>.onListChange(listener: (ListChangeListener.Change<out 
  * The given function is called for the current value and all new values
  * Calls the function immediately
  */
-fun <T> ObservableValue<T>.consumeImmediately(function: (T) -> Unit) {
-  consume(true, function)
+fun <T> ObservableValue<T>.consumeImmediately(function: (T) -> Unit): Disposable {
+  return consume(true, function)
 }
 
 /**
  * The given function is called for all new values.
  * Calls the function immediately if [immediately] is set to true
  */
-fun <T> ObservableValue<T>.consume(immediately: Boolean = false, function: (T) -> Unit) {
-  addListener { _, _, newValue ->
+fun <T> ObservableValue<T>.consume(immediately: Boolean = false, function: (T) -> Unit): Disposable {
+  val listener: (observable: ObservableValue<out T>, oldValue: T, newValue: T) -> Unit = { _, _, newValue ->
     function(newValue)
   }
 
+  addListener(listener)
+
+  //Call the listener immediately (if requested)
   if (immediately) {
     function(value)
+  }
+
+  //Returns a disposable that can be used to unregister the listener
+  return Disposable {
+    removeListener(listener)
   }
 }
 
@@ -61,7 +70,16 @@ fun <T> ObservableValue<T>.consume(immediately: Boolean = false, function: (T) -
  * Creates a binding with a map function
  */
 fun <T, R> ObservableValue<T>.map(conversion: (T) -> R): ObservableValue<R> {
-  return Bindings.createObjectBinding(Callable {
+  return Bindings.createObjectBinding({
     conversion(this.value)
   }, this)
+}
+
+/**
+ * Creates a binding combining two observable values
+ */
+fun <T1, T2, R> ObservableValue<T1>.map(other: ObservableValue<T2>, conversion: (T1, T2) -> R): ObservableValue<R> {
+  return Bindings.createObjectBinding({
+    conversion(this.value, other.value)
+  }, this, other)
 }
