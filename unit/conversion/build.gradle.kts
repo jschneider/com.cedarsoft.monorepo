6a2a6fe71e8bbd4d0a3d83f4a1b6f2149e21c0d2
Robot <robot@cedarsoft.com>
2021-09-08 02:29:05 +0000

    publishing updated sources

---
+++ unit/conversion/build.gradle.kts
@@ -0,0 +1,62 @@
+import Libs.commons_io
+
+description = """Unit Conversions"""
+
+group = "com.cedarsoft.unit"
+
+plugins {
+  // Apply the java-library plugin to add support for Java Library
+  kotlinMultiPlatform
+  //dokka
+}
+
+kotlin {
+  jvm {
+  }
+
+  js {
+    nodejs()
+  }
+
+  sourceSets {
+    commonMain {
+      dependencies {
+        api(Kotlin.stdlib.common)
+        api(project(Projects.open_unit_unit))
+      }
+    }
+
+    named("jvmMain") {
+      dependencies {
+        api(project(Projects.dependencies_sets_kotlin_jvm))
+        api(project(Projects.open_annotations))
+      }
+    }
+
+    named("jvmTest") {
+      dependencies {
+        api(project(Projects.dependencies_sets_kotlin_jvm))
+
+        implementation(Libs.kotlin_test)
+        implementation(Libs.kotlin_test_junit)
+
+        implementation(project(Projects.dependencies_sets_kotlin_test))
+        implementation(project(Projects.open_commons_test_utils))
+
+        implementation(commons_io)
+        implementation(Libs.assertj_core)
+
+      }
+    }
+    named("jsMain") {
+      dependencies {
+        api(project(Projects.dependencies_sets_kotlin_js))
+      }
+    }
+    //named("jsTest") {
+    //  dependencies {
+    //    implementation(Libs.kotlin_test_js)
+    //  }
+    //}
+  }
+}
